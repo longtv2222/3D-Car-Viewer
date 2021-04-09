01a93b3ed5e14658389a88fdc70b518f1d361725
longtv2222 <long.tv2222@gmail.com>
2021-04-09 12:07:04 -0600

    Code refactoring

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react'
-import { Canvas, useThree } from 'react-three-fiber'
+import { Canvas, useThree, useLoader } from 'react-three-fiber'
 import { OrbitControls, ContactShadows } from 'drei'
 import { proxy, } from 'valtio'
 import Model from './Scene.js'
 import * as THREE from 'three'
 import './App.css'
 import ColorPicker from './ColorPicker.js'
+import { TextureLoader } from 'three'
 
 const state = proxy({
   current: null,
@@ -15,16 +16,28 @@ const state = proxy({
   }
 })
 
+function Environment() {
+  const { scene, gl } = useThree()
+  const texture = useLoader(TextureLoader, 'venice_sunset.jpg')
+  let pmremGenerator = new THREE.PMREMGenerator(gl);
+  pmremGenerator.compileEquirectangularShader();
+  let envMap = pmremGenerator.fromEquirectangular(texture).texture;
+  scene.environment = envMap;
+  scene.background = envMap
+  return null
+}
+
 function Ground(props) {
   const { scene } = useThree()
   scene.background = new THREE.Color(0xeeeeee)
-  // scene.fog = new THREE.Fog(0xeeeeee, 10, 50)
+  scene.fog = new THREE.Fog(0xeeeeee, 10, 50)
   scene.background = new THREE.Color(0xffffff)
   const grid = new THREE.GridHelper(100, 40, 0x000000, 0x000000)
   grid.material.opacity = 0.3
   grid.material.depthWrite = true
   grid.material.transparent = false
   grid.receiveShadow = true
+
   scene.add(grid)
 
   return null
@@ -37,18 +50,12 @@ function App() {
     <>
       <ColorPicker />
       <Canvas>
-        {/* <ambientLight intensity={1} /> */}
-        <spotLight color={'#FFFFFF'} intensity={1} angle={Math.PI / 4} penumbra={1} position={[0, 20, 0]} castShadow={true} />
-        <pointLight intensity={1} position={[40, 0, 0]} />
-        <pointLight intensity={1} position={[0, 0, 40]} />
-        <pointLight intensity={1} position={[0, 20, 0]} />
-        <directionalLight position={[0, -10, 0]} />
         <Suspense fallback={null}>
-          <Model />
+          <Environment />
           <Ground />
-          <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={0.25} width={10} height={10} blur={2} far={1} />
+          <Model />
+          <OrbitControls maxPolarAngle={Math.PI / 2} />
         </Suspense>
-        <OrbitControls maxPolarAngle={Math.PI / 2} />
       </Canvas>
     </>
   )
