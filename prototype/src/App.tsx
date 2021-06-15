9be0ad37bb078cbea1e6a58e91356e6805e4aedd
longtv2222 <long.tv2222@gmail.com>
2021-06-15 17:50:03 -0600

    First step in migrating project to typescript

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -1,10 +1,10 @@
-import React, { Suspense, useState } from 'react'
+import { Suspense, useState } from 'react'
 import { Canvas, useThree, useLoader } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Model from './Models/Scene.jsx'
 import * as THREE from 'three'
 import './App.css'
-import ColorPicker from './ColorPicker.jsx'
+import ColorPicker from './ColorPicker'
 import { TextureLoader } from 'three'
 import Larmborghini from './Models/Lamborghini.jsx'
 import { proxy } from 'valtio'
@@ -25,8 +25,11 @@ function Ground() {
   scene.background = new THREE.Color(0xeeeeee);
   scene.background = new THREE.Color(0xffffff);
   const grid = new THREE.GridHelper(100, 40, 0x000000, 0x000000);
+  //@ts-ignore
   grid.material.opacity = 0.3;
+  //@ts-ignore
   grid.material.depthWrite = true;
+  //@ts-ignore
   grid.material.transparent = false;
   grid.receiveShadow = true;
 
@@ -59,6 +62,7 @@ function App() {
           <Environment />
           <Ground />
           <Model visibility={arr[index]} myState={state} />
+          {/*// @ts-ignore */}
           <Larmborghini visibility={arr[index]} myState={state} />
           <OrbitControls maxPolarAngle={7 * Math.PI / 18} />
         </Suspense>
--- prototype/src/ColorPicker.tsx
+++ prototype/src/ColorPicker.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { TwitterPicker } from 'react-color'
-import { state } from './App.js'
+import { state } from './App'
 
-export default function ColorPicker(props) {
+export default function ColorPicker(props: any) {
     const [visibility1, setVisibility] = useState(false);
     const [visibility2, setVisibility2] = useState(false);
     const interiorColors = ['#000000', '#EECCAA', '#808080', '#962']
@@ -11,19 +11,21 @@ export default function ColorPicker(props) {
         <div className="picker">
             <button className="button" onClick={() => setVisibility(!visibility1)}>
                 <br />Interior
-          <TwitterPicker colors={interiorColors} className={visibility1 ? "padding" : "padding-hidden"} width={400} onChange={(color) => { state.items.interior = color.hex }} />
+                {/*// @ts-ignore */}
+                <TwitterPicker colors={interiorColors} className={visibility1 ? "padding" : "padding-hidden"} width={400} onChange={(color: any) => { state.items.interior = color.hex }} />
             </button>
 
             <button className="button" onClick={() => setVisibility2(!visibility2)}>
                 <br />
-          Exterior
-           <TwitterPicker colors={exteriorColors} className={visibility2 ? "padding" : "padding-hidden"} width={400} onChange={(color) => { state.items.exterior = color.hex }} />
+                Exterior
+                {/*// @ts-ignore */}
+                <TwitterPicker colors={exteriorColors} className={visibility2 ? "padding" : "padding-hidden"} width={400} onChange={(color: any) => { state.items.exterior = color.hex }} />
             </button>
 
             <button className={"button"} onClick={() => props.passedFunction()} >Switch Car</button>
             <button className={"button"}
                 type="button"
-                onClick={(e) => {
+                onClick={() => {
                     window.location.href = 'https://www.youtube.com/watch?v=ttHlj9l6tJo';
                 }}
             >Finish</button>
