329d611b7a863075cd79307853fbbb288946051a
longtv2222 <long.tv2222@gmail.com>
2021-07-01 23:40:43 -0600

    Added Bucket Deployment to deploy this application to s3

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -18,4 +18,5 @@ new InfrastructureStack(app, 'InfrastructureStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  
 });
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,27 @@
 import * as cdk from '@aws-cdk/core';
+import { ObjectOwnership, Bucket } from '@aws-cdk/aws-s3';
+import { BucketDeployment, Source, StorageClass } from '@aws-cdk/aws-s3-deployment';
+import { join } from "path";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const carViewerBucket = new Bucket(this, 'CarViewerBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      bucketName: "3d-car-viewer-s3-asset",
+      objectOwnership: ObjectOwnership.BUCKET_OWNER_PREFERRED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    new BucketDeployment(this, 'CarViewerWebsite', {
+      sources: [Source.asset(join(__dirname, "..", "..", "prototype", "build"))],
+      destinationBucket: carViewerBucket,
+      storageClass: StorageClass.STANDARD,
+      destinationKeyPrefix: 'web/static'
+    });
     // The code that defines your stack goes here
   }
 }
