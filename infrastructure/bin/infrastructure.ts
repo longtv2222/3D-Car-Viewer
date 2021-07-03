b909a0fd91cb1be0332d38ae15f7ce77b9a2d693
longtv2222 <long.tv2222@gmail.com>
2021-07-03 16:32:06 -0600

    Lock region in us-east-2

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -7,6 +7,6 @@ const app = new cdk.App();
 new InfrastructureStack(app, 'InfrastructureStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
+    region: 'us-east-2'
   },
 });
