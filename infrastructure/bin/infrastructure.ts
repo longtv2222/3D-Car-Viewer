12286a52d2b9c617227b332dce497fdad4ae382a
Long Viet Ta <long.tv2222@gmail.com>
2022-12-19 11:58:16 -0700

    Refactor CDK code for website

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
-import { CarViewerFrontEndStage } from "./prod-stage"
+import { CarViewerStage } from "./stage"
 
 const app = new App();
 
-new CarViewerFrontEndStage(app, "CarViewerProductionStage", {
+new CarViewerStage(app, "CarViewerProd", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  },
-  dnsProps: {
-    domainName: "3d.themagehub.com",
-    zoneName: "themagehub.com",
+    region: "us-east-1"
   },
 })
 
