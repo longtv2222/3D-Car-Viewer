d76b3e16c10499d97c74e2f9545bdae5315cd716
Long Viet Ta <long.tv2222@gmail.com>
2023-12-17 02:39:35 -0700

    feat: new ui

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -1,29 +1,35 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, Loader, OrbitControls, Stats } from "@react-three/drei"
 import Lamborghini from "./components/Models/Lamborghini"
+import Scene from "./components/Models/Scene"
 import { useControls } from 'leva'
+import { Suspense } from 'react'
 
 export default function App() {
   const { Interior, Exterior, rotation, hideStats, select } = useControls({
     select: { options: ['Lamborghini', 'Scene'] },
+    Interior: '#aa5252',
+    Exterior: '#9a9898',
     rotation: false,
     hideStats: false,
-    Interior: '#aa5252',
-    Exterior: '#9a9898'
   });
+
   return (
-    <div className='h-screen'>
+    <>
       <Canvas camera={{ position: [0, 0, 10] }} shadows={true} frameloop="demand">
+        <Suspense fallback={null}>
+          {select === "Lamborghini" && <Lamborghini interior={Interior} exterior={Exterior} />}
+          {select === "Scene" && <Scene interior={Interior} exterior={Exterior} />}
+        </Suspense>
         <Environment
           background
           files={'venice_sunset_1k.hdr'}
           blur={0.5}
         />
         {!hideStats ? <Stats /> : undefined}
-        {select === "Lamborghini" && <Lamborghini interior={Interior} exterior={Exterior} />}
         <OrbitControls maxPolarAngle={7 * Math.PI / 18} maxDistance={20} autoRotate={rotation} />
       </Canvas>
       <Loader />
-    </div>
+    </>
   )
 }
--- prototype/src/index.tsx
+++ prototype/src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App'
+import "./styles.css"
 
 const container = document.getElementById('root');
 if (!container)
