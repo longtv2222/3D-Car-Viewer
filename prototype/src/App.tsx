a20415c83bcc98c3ae043eea6f3df717c0341a56
Long Viet Ta <long.tv2222@gmail.com>
2023-12-17 01:44:47 -0700

    feat: cleanup some files and add new UI

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -1,16 +1,29 @@
-import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Loader } from "@react-three/drei/web"
+import { Environment, Loader, OrbitControls, Stats } from "@react-three/drei"
+import Lamborghini from "./components/Models/Lamborghini"
+import { useControls } from 'leva'
 
 export default function App() {
+  const { Interior, Exterior, rotation, hideStats, select } = useControls({
+    select: { options: ['Lamborghini', 'Scene'] },
+    rotation: false,
+    hideStats: false,
+    Interior: '#aa5252',
+    Exterior: '#9a9898'
+  });
   return (
-    <React.Fragment>
-      {/* <Canvas camera={{ position: [0, 0, 10] }} shadows={true} frameloop="demand">
-        <Suspense fallback={null}>
-        </Suspense>
+    <div className='h-screen'>
+      <Canvas camera={{ position: [0, 0, 10] }} shadows={true} frameloop="demand">
+        <Environment
+          background
+          files={'venice_sunset_1k.hdr'}
+          blur={0.5}
+        />
+        {!hideStats ? <Stats /> : undefined}
+        {select === "Lamborghini" && <Lamborghini interior={Interior} exterior={Exterior} />}
+        <OrbitControls maxPolarAngle={7 * Math.PI / 18} maxDistance={20} autoRotate={rotation} />
       </Canvas>
-      <Loader /> */}
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
-    </React.Fragment>
+      <Loader />
+    </div>
   )
 }
