fe9096d012abed705793b29ad9abfd9b8bbed392
Long Viet Ta <long.tv2222@gmail.com>
2023-12-17 11:26:47 -0700

    fix: only show control when done loading

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -1,35 +1,39 @@
 import { Canvas } from '@react-three/fiber'
-import { Environment, Loader, OrbitControls, Stats } from "@react-three/drei"
+import { Environment, Loader, OrbitControls, Stats, useProgress } from "@react-three/drei"
 import Lamborghini from "./components/Models/Lamborghini"
 import Scene from "./components/Models/Scene"
-import { useControls } from 'leva'
+import { Leva, useControls } from 'leva'
 import { Suspense } from 'react'
 
 export default function App() {
-  const { Interior, Exterior, rotation, hideStats, select } = useControls({
-    select: { options: ['Lamborghini', 'Scene'] },
+  const { Interior, Exterior, Rotation, Select, Stats:stats } = useControls({
+    Select: { options: ['Lamborghini', 'Scene'] },
     Interior: '#aa5252',
     Exterior: '#9a9898',
-    rotation: false,
-    hideStats: false,
+    Rotation: false,
+    Stats: true,
   });
+  const { progress } = useProgress()
 
   return (
     <>
       <Canvas camera={{ position: [0, 0, 10] }} shadows={true} frameloop="demand">
         <Suspense fallback={null}>
-          {select === "Lamborghini" && <Lamborghini interior={Interior} exterior={Exterior} />}
-          {select === "Scene" && <Scene interior={Interior} exterior={Exterior} />}
+          {Select === "Lamborghini" && <Lamborghini interior={Interior} exterior={Exterior} />}
+          {Select === "Scene" && <Scene interior={Interior} exterior={Exterior} />}
         </Suspense>
         <Environment
           background
           files={'venice_sunset_1k.hdr'}
           blur={0.5}
         />
-        {!hideStats ? <Stats /> : undefined}
-        <OrbitControls maxPolarAngle={7 * Math.PI / 18} maxDistance={20} autoRotate={rotation} />
+        {stats ? <Stats /> : undefined}
+        <OrbitControls maxPolarAngle={7 * Math.PI / 18} maxDistance={20} autoRotate={Rotation} />
       </Canvas>
       <Loader />
+      <Leva
+        hidden={progress === 100 ? false : true}
+      />
     </>
   )
 }
